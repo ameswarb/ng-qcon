a4828caf61404e4cd78d8f456cdd65e39bc97d15
Alex Meswarb <alex.meswarb@firehost.com>
2014-03-20 22:22:44 -0500

    Add popovers

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var app = angular.module('ngQconApp', []);
+var app = angular.module('ngQconApp');
 
 app.factory('seatService', function($http) {
   var seatService = {
@@ -16,11 +16,57 @@ app.factory('seatService', function($http) {
   return seatService;
 });
 
-app.directive('seats', ['$rootScope', function ($rootScope) {
+app.directive('singleseat', function () {
+  var showTooltip = function () {
+    console.log('show tooltip!');
+  };
+  return {
+    restrict: 'A',
+    scope: {},
+    template: '',
+    link: function (scope, element, attrs) {
+      if (attrs.seatState !== 'taken') { return; }
+      element.hover(function () {
+        var options = {
+          'animation': false,
+          'html': true,
+          'delay': {
+            show: 0,
+            hide: 0
+          }
+        };
+        options.title = attrs.seatIdent;
+        if (attrs.seatRow > 37) {
+          options.placement = 'left';
+        } else {
+          options.placement = 'right';
+        }
+        options.content = '';
+        if (attrs.seatUser) {
+          options.content += '<strong>' + attrs.seatUser + '</strong><br />';
+        }
+        if (attrs.seatClan) {
+          options.content +=  attrs.seatClan;
+        }
+        element.popover(options);
+        element.popover('show');
+        window.jQuery('[seat-nr="' + attrs.seatNr +'"]' +
+                    '[seat-group="' + attrs.seatGroup + '"]')
+                    .addClass('focus');
+      }, function () {
+        element.popover('destroy');
+        window.jQuery('[seat-nr="' + attrs.seatNr +'"]' +
+                    '[seat-group="' + attrs.seatGroup + '"]')
+                    .removeClass('focus');
+      });
+    }
+  };
+});
+
+app.directive('seats', ['$rootScope', '$compile', function ($rootScope, $compile) {
 
   var seatsAsync = function (group, startRow, endRow, scope, element) {
     $rootScope.$on('seatsReady', function (event, message) {
-      console.log('broadcast caught');
       drawSeats(message, group, startRow, endRow, scope, element);
     });
   };
@@ -39,8 +85,9 @@ app.directive('seats', ['$rootScope', function ($rootScope) {
         break;
     }
 
+    scope.markup = '';
     for (var row = startRow; row <= endRow; row++) {
-      scope.markup += '<div class="seat-row';
+      scope.markup += '<div ng-mouseenter="tooltip()" class="seat-row';
       if (row % 2 === 0) {
         scope.markup += ' aisle';
       }
@@ -58,13 +105,17 @@ app.directive('seats', ['$rootScope', function ($rootScope) {
         scope.markup += 'seat-group="' + group + '" ' +
                   'seat-row="' + row + '" ' +
                   'seat-nr="' + seat + '" ' +
-                  'seat-ident="' + seatIdent + '" ';
+                  'seat-ident="' + seatIdent + '" ' +
+                  'singleseat="" ';
         if (seatObj) {
           if (seatObj.username) {
             scope.markup += 'seat-user="' + seatObj.username + '" ';
           }
           if (seatObj.clan) {
-            scope.markup += 'seat-user="' + seatObj.clan + '" ';
+            scope.markup += 'seat-clan="' + seatObj.clan + '" ';
+          }
+          if (seatObj.state) {
+            scope.markup += 'seat-state="' + seatObj.state + '" ';
           }
         }
         scope.markup += '></div>';
@@ -73,6 +124,7 @@ app.directive('seats', ['$rootScope', function ($rootScope) {
     }
     scope.markup += '<div class="clear"></div>';
     element.html(scope.markup);
+    $compile(element.contents())(scope);
   };
 
   return {
@@ -88,9 +140,8 @@ app.directive('seats', ['$rootScope', function ($rootScope) {
 app.controller('MainCtrl',
                ['$scope', '$rootScope', 'seatService',
                 function ($scope, $rootScope, seatService) {
-  seatService.async().then(function (d) {
-    $scope.data = d.seats;
-    console.log('~~~ broadcast ~~~');
-    $rootScope.$broadcast('seatsReady', d.seats);
-  });
-}]);
+    seatService.async().then(function (d) {
+      $scope.data = d.seats;
+      $rootScope.$broadcast('seatsReady', d.seats);
+    });
+  }]);
